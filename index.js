130833f2d525653bc172306a044b3e2aee51b1bf
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-05-26 17:15:37 -0300

    Iniciando Projeto

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
+app.get('/', (_request, response) => {
+  response.send('PROJETO BLOG API');
 });
+
+app.use((err, _req, res, _next) => {
+  const { status, message } = err;
+  res.status(status).json({
+    message,
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
